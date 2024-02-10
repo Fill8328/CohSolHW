8fe62ae5f1a70e17ec4852e0450d7839b5498072
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2024-02-10 18:27:56 +0100

    task 1-2v4

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(fromSixteenScaleToTen.translateFromSixteenScaleToTen("123A.0", 13));
-        System.out.println(fromTenScaleTo.fromTenScaleTo(123.123, 16));
-       // fromSixteenScaleToTen.translateFromSixteenScaleToTen("263", 14);
-       // fromSixteenScaleToTen.translateFromSixteenScaleToTen("128", 14);
-        System.out.println(fromTenScaleTo.fromTenScaleTo(701, 14));
-
+      //  System.out.println(fromSixteenScaleToTen.translateFromSixteenScaleToTen("123A.0", 13));
+      //  System.out.println(fromTenScaleTo.fromTenScaleTo(123.123, 16));
+        // fromSixteenScaleToTen.translateFromSixteenScaleToTen("263", 14);
+        // fromSixteenScaleToTen.translateFromSixteenScaleToTen("128", 14);
+      //  System.out.println(fromTenScaleTo.fromTenScaleTo(701, 14));
+        convertScale.convertScale("A12.A12",16,36);
     }
 
-//    public static double manageRadixOrigin(Object number, int radixOrigin, int radixToTranslate) {
-//
-//        return numberTranslate;
-//    }
 }
 
